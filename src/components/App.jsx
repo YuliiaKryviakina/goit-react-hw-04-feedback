3ba19d3e129b6ba35ff3a78436e5a7be50649cfe
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-07-20 16:36:13 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { Statistics } from "./Statistics/Statistics";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Section } from "./Section/Section";
-import { NotificationMessage } from "./NotificationMessage/NotificationMessage";
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { NotificationMessage } from './NotificationMessage/NotificationMessage';
 
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleFeedback = (option) => {
+  const handleFeedback = option => {
     switch (option) {
-      case "good":
-        setGood((state) => state + 1);
+      case 'good':
+        setGood(state => state + 1);
         break;
 
-      case "neutral":
-        setNeutral((state) => state + 1);
+      case 'neutral':
+        setNeutral(state => state + 1);
         break;
 
-      case "bad":
-        setBad((state) => state + 1);
+      case 'bad':
+        setBad(state => state + 1);
         break;
 
       default:
@@ -37,22 +37,23 @@ export const App = () => {
   };
 
   const totalFeedback = countTotalFeedback();
+  const percentage = countPositiveFeedbackPercentage();
 
   return (
     <div
       style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
         fontSize: 40,
-        color: "#010101",
+        color: '#010101',
       }}
     >
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={["good", "neutral", "bad"]}
+          options={['good', 'neutral', 'bad']}
           onLeaveFeedback={handleFeedback}
         />
       </Section>
@@ -64,7 +65,7 @@ export const App = () => {
             neutral={neutral}
             bad={bad}
             total={totalFeedback}
-            positivePercentage={countPositiveFeedbackPercentage}
+            positivePercentage={percentage}
           />
         ) : (
           <NotificationMessage message="There is no feedback" />
